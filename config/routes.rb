5aea2b32e194b6faae60d8001ec49d30c655659b
Yoshi-55 <ysmr9855@gmail.com>
2025-07-03 00:25:58 +0900

    add入力補完機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :sake_logs
   get "posts/index"
+
+  get "/api/brands", to: "brands#proxy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
