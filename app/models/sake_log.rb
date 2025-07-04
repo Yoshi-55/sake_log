10f90439725e7fc184a07bb328f26e7bae7b6375
Yoshi-55 <ysmr9855@gmail.com>
2025-07-05 01:45:33 +0900

    入力フォーム修正

--- app/models/sake_log.rb
+++ app/models/sake_log.rb
@@ -1,3 +1,7 @@
 class SakeLog < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :taste, length: { maximum: 20 }
+  validates :memo, length: { maximum: 80 }
 end
