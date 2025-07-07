54bb730ed77081aa0b2472456684b91901712ea0
Yoshi-55 <ysmr9855@gmail.com>
2025-07-07 23:44:32 +0900

    不要なコメント削除

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,12 +1,3 @@
 class TopController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :guide, :howto ]
-
-  def index
-  end
-
-  def guide
-  end
-
-  def howto
-  end
 end
